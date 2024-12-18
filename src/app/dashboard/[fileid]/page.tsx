c818212d11303e40b876ad0d21bb1b8f9fad25a3
bhansaliMadhav <bhansalimadhav03@gmail.com>
2024-12-19 00:01:13 +0530

    cheackout

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -1,10 +1,34 @@
+import { trpc } from "@/app/_trpc/client";
+import { db } from "@/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { notFound, redirect } from "next/navigation";
+
 interface PageProps {
   params: {
     fileid: string;
   };
 }
-const Page = ({ params }: PageProps) => {
+const Page = async ({ params }: PageProps) => {
   const fileid = params.fileid;
-  return <div>File</div>;
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
+  if (!user || !user.id) redirect(`auth-callback?origin=dashboard/${fileid}`);
+  const file = await db.file.findFirst({
+    where: {
+      id: fileid,
+      userId: user.id,
+    },
+  });
+  if (!file) notFound();
+  return (
+    <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
+      <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
+        {/* {left side} */}
+        <div className="flex-1 xl:flex">
+          <div className="px-4 py-6 sm:px-6"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Page;
