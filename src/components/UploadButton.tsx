0a434dcb623103ae2f57444de38f8c39cb023dcc
bhansalimadhav <bhansalimadhav03@gmail.com>
2024-12-19 18:40:02 +0530

    initiated dropzone for uploading pdf

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -1,15 +1,45 @@
 "use client";
 
 import { useState } from "react";
-import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
+import { Dialog, DialogContent, DialogTitle, DialogTrigger } from "./ui/dialog";
 import { Button } from "./ui/button";
 import Dropzone from "react-dropzone";
+import { Cloud } from "lucide-react";
 
 const UploadDropZone = () => {
   return (
-    <Dropzone multiple={false}>
+    <Dropzone
+      multiple={false}
+      onDrop={(acceptedFile) => {
+        console.log(acceptedFile);
+      }}
+    >
       {({ getRootProps, getInputProps, acceptedFiles }) => (
-        <div {...getRootProps()}></div>
+        <div
+          {...getRootProps()}
+          className="border h-64 m-4 border-dashed border-gray-300 rounded-lg"
+        >
+          <div className="flex items-center justify-center h-full w-full">
+            <label
+              htmlFor="dropzone-file"
+              className="flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
+            >
+              <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                <Cloud className="h-6 w-6 text-zinc-500 mb-2" />
+                <p className="mb-2 text-sm text-zinc-700">
+                  <span className="font-semibold">Click to upload</span> or drag
+                  and drop
+                </p>
+                <p className="text-xs text-zinc-500">PDF (up to 4MB)</p>
+              </div>
+              {acceptedFiles && acceptedFiles[0] ? (
+                <div className="max-w-xs bg-white flex items-center rounded-md overflow-hidden outline outline-[1px] outline-zinc-200 divide-x divide-zinc-200"></div>
+              ) : (
+                ""
+              )}
+            </label>
+          </div>
+        </div>
       )}
     </Dropzone>
   );
@@ -33,6 +63,7 @@ const UploadButton = () => {
         <Button>Upload PDF</Button>
       </DialogTrigger>
       <DialogContent>
+        <DialogTitle>Drag or Drop File here</DialogTitle>
         <UploadDropZone />
       </DialogContent>
     </Dialog>
