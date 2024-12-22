64c27de0034ad4c599165d0acd3c91298aab6c0d
bhansaliMadhav <bhansalimadhav03@gmail.com>
2024-12-22 18:48:17 +0530

    checkout

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -4,9 +4,8 @@ import { pinecone } from "@/lib/pinecone";
 import { sendMessageValidator } from "@/lib/validators/sendMessageValidator";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-import {} from "langchain/embeddings/hf";
 import { PineconeStore } from "langchain/vectorstores/pinecone";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { OpenAIStream, StreamingTextResponse } from "ai";
 
 export const POST = async (req: NextRequest) => {
@@ -37,72 +36,81 @@ export const POST = async (req: NextRequest) => {
   });
 
   // 1: vectorize message
-  const embeddings = new OpenAIEmbeddings({
-    openAIApiKey: process.env.OPENAI_API_KEY,
-  });
+  // const embeddings = new OpenAIEmbeddings({
+  //   openAIApiKey: process.env.OPENAI_API_KEY,
+  // });
 
-  const pineconeIndex = pinecone.Index("jarvis");
-  const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
-    pineconeIndex,
-    namespace: file.id,
-  });
-  const results = await vectorStore.similaritySearch(message, 4);
-  const prevMessages = await db.message.findMany({
-    where: {
-      fileId,
-    },
-    orderBy: {
-      createdAt: "asc",
-    },
-    take: 6,
-  });
+  // const pineconeIndex = pinecone.Index("jarvis");
+  // const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+  //   pineconeIndex,
+  //   namespace: file.id,
+  // });
+  // const results = await vectorStore.similaritySearch(message, 4);
+  // const prevMessages = await db.message.findMany({
+  //   where: {
+  //     fileId,
+  //   },
+  //   orderBy: {
+  //     createdAt: "asc",
+  //   },
+  //   take: 6,
+  // });
 
-  const formattedPrevMessages = prevMessages.map((msg) => ({
-    role: msg.isUserMessage ? ("user" as const) : ("assistant" as const),
-    content: msg.text,
-  }));
-  const respone = await openai.chat.completions.create({
-    model: "gpt-4",
-    temperature: 0,
-    stream: true,
-    messages: [
-      {
-        role: "system",
-        content:
-          "Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format.",
-      },
-      {
-        role: "user",
-        content: `Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format. \nIf you don't know the answer, just say that you don't know, don't try to make up an answer.
-        
-  \n----------------\n
-  
-  PREVIOUS CONVERSATION:
-  ${formattedPrevMessages.map((message) => {
-    if (message.role === "user") return `User: ${message.content}\n`;
-    return `Assistant: ${message.content}\n`;
-  })}
-  
-  \n----------------\n
-  
-  CONTEXT:
-  ${results.map((r) => r.pageContent).join("\n\n")}
-  
-  USER INPUT: ${message}`,
-      },
-    ],
-  });
-  const stream = OpenAIStream(respone, {
-    async onCompletion(completion) {
-      await db.message.create({
-        data: {
-          text: completion,
-          isUserMessage: false,
-          fileId,
-          userId,
-        },
-      });
+  // const formattedPrevMessages = prevMessages.map((msg) => ({
+  //   role: msg.isUserMessage ? ("user" as const) : ("assistant" as const),
+  //   content: msg.text,
+  // }));
+  // const respone = await openai.chat.completions.create({
+  //   model: "gpt-4",
+  //   temperature: 0,
+  //   stream: true,
+  //   messages: [
+  //     {
+  //       role: "system",
+  //       content:
+  //         "Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format.",
+  //     },
+  //     {
+  //       role: "user",
+  //       content: `Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format. \nIf you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+  // \n----------------\n
+
+  // PREVIOUS CONVERSATION:
+  // ${formattedPrevMessages.map((message) => {
+  //   if (message.role === "user") return `User: ${message.content}\n`;
+  //   return `Assistant: ${message.content}\n`;
+  // })}
+
+  // \n----------------\n
+
+  // CONTEXT:
+  // ${results.map((r) => r.pageContent).join("\n\n")}
+
+  // USER INPUT: ${message}`,
+  //     },
+  //   ],
+  // });
+  // const stream = OpenAIStream(respone, {
+  //   async onCompletion(completion) {
+  //     await db.message.create({
+  //       data: {
+  //         text: completion,
+  //         isUserMessage: false,
+  //         fileId,
+  //         userId,
+  //       },
+  //     });
+  //   },
+  // });
+  // return new StreamingTextResponse(stream);
+  const sendBody = {
+    message: "Success",
+  };
+
+  return new NextResponse(JSON.stringify(sendBody), {
+    headers: {
+      "Content-Type": "application/json",
     },
   });
-  return new StreamingTextResponse(stream);
 };
