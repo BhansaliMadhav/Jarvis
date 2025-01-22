e9ac3d834428dfc7ae58069b13c118a642e610a1
bhansaliMadhav <bhansalimadhav03@gmail.com>
2025-01-22 19:41:09 +0530

    updated config file to remove some issues

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "gravatar.com",
+        pathname: "/avatar/**",
+      },
+    ],
+  },
   async redirects() {
     return [
       { source: "/sign-in", destination: "/api/auth/login", permanent: true },
